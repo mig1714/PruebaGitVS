3be213bc2a2a17b61e8a21941c23d905a8d9f9c1
mig1714 <migsotoa@gmail.com>
2024-04-12 14:15:52 -0600

    Agregar archivos de proyecto.

---
+++ PruebaGIT/Principal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PruebaGIT
+{
+    public class Principal
+    {
+        static public void Main(String[] args)
+        {
+            Console.WriteLine("Hola Git");
+            Console.ReadLine();
+        }
+
+    }
+}
